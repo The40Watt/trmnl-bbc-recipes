692b0b11c77b1eb47b3b612441703acfcfb2030e
The40Watt <stlennon21@gmail.com>
2026-08-04 21:33:06 +0100

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -9,8 +9,8 @@ import os
 
 TRMNL_WEBHOOK_URL = os.environ["TRMNL_WEBHOOK_URL"]
 
-#URL = "https://www.bbcgoodfood.com/recipes/chicken-chorizo-jambalaya"
 URL = "https://www.bbcgoodfood.com/recipes/creamy-chicken-pasta"
+
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0"
